a7030f4473592f653938eb77f130592e63e14972
Richard Feng <richardfeng43@gmail.com>
2022-04-07 08:10:11 -0700

    dwasd

---
+++ scrip.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    createFlash.addEventListener('click',function(){
+        SendStatement();
+    })
+})
+
+function SendStatement(){
+    alert("Created Flashcards")
+}
